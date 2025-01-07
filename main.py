7711bba753d37312397f190cb17adc3317a14418
Zahra <zmohseni20@yahoo.com>
2025-01-07 18:55:47 +0330

    Introdoction Compelete!

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ pygame.display.set_caption("Bug Debugger")
 icone = pygame.image.load("virus.png")
 pygame.display.set_icon(icone)
 
-# Player
+# Introdoction
+IntroBack = pygame.image.load("Introback.png")
+IntroImg = pygame.image.load("search.png")
+Customsize = (256, 256)
+IntroImg = pygame.transform.scale(IntroImg, Customsize)
+IntroTxt = pygame.image.load("introdoc.png")
+# IntroTxt = pygame.transform.scale(IntroTxt, (512, 512))
+IntroX = 90
+IntroY = 395
+IntroTxtX = -800
+IntroTxtY = 75
 
-PlayerImg = pygame.image.load("search.png")
-PlayerX = 0
-PlayerY = 125
 
-
-def Player():
-    screen.blit(PlayerImg, (PlayerX, PlayerY))
-
-
-def FirstpageAnimation(x, y):
-    screen.blit(PlayerImg, (x, y))
+def FirstpageAnimation(x, y, z, n):
+    screen.blit(IntroImg, (x, y))
+    screen.blit(IntroTxt, (z, n))
 
 
 # Game loop
@@ -30,13 +33,19 @@ while running:
 
     # RGB - Red, Green, Blue, transparency
     screen.fill((76, 0, 153, 0.6))
+    screen.blit(IntroBack, (0, 0))
 
-    PlayerX += 0.5
+    if IntroX < 880:
+        IntroX += 4
+        IntroTxtX += 4
+    else:
+        IntroX += 0.0
+        IntroTxtX += 0.0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    FirstpageAnimation(PlayerX, PlayerY)
+    FirstpageAnimation(IntroX, IntroY, IntroTxtX, IntroTxtY)
 
     pygame.display.update()
